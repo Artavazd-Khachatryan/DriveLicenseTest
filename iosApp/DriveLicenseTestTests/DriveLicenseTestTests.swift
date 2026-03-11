94c981d3195836561cb25bd923318f6aa01fb55b
Artavazd Khachatryan <artavazd575@gmail.com>
2026-03-11 19:45:20 +0400

    Add iOS tests

---
+++ iosApp/DriveLicenseTestTests/DriveLicenseTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  DriveLicenseTestTests.swift
+//  DriveLicenseTestTests
+//
+//  Created by Artavazd Khachatryan on 11.03.26.
+//  Copyright © 2026 orgName. All rights reserved.
+//
+
+import Testing
+
+struct DriveLicenseTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
